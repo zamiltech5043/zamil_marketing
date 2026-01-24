1c663557d2992f0c9f23b5809c526562c8c6a8f2
Muzamil Abbas <muzamilabbastecrix@gmail.com>
2026-01-24 14:38:59 +0500

    Correct capitalization in Hero component heading

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -48,10 +48,10 @@ export const Hero: React.FC<HeroProps> = ({ onGetProposal }) => {
             </motion.span>
             
             <motion.h1 variants={fadeInUp} className="text-5xl lg:text-6xl font-heading font-black leading-[1.1] mb-8">
-              "360-degree marketing solutions for
+              "360-Degree Marketing Solutions for
  <br />
               <span className="relative inline-block">
-                       interior design & decore services"
+                       Interior Design & Decore "
                 <motion.svg 
                   initial={{ pathLength: 0, opacity: 0 }}
                   animate={{ pathLength: 1, opacity: 1 }}
